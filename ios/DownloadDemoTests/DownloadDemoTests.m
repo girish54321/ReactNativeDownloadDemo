b80fafdbeba22dddf920b7dbbefc2353bad1a8fa
Girish Parate <girishparate41@gmail.com>
2022-01-26 20:14:24 +0530

    Udpate app name

--- ios/DownloadDemoTests/DownloadDemoTests.m
+++ ios/DownloadDemoTests/DownloadDemoTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface StarterTests : XCTestCase
+@interface DownloadDemoTests : XCTestCase
 
 @end
 
-@implementation StarterTests
+@implementation DownloadDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
